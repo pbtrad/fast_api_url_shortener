2a191d73b9e6cc2474a492c7fb02c68eeffa98d3
paul <paulbrowne2@gmail.com>
2022-05-23 22:51:56 +0100

    Added @lru_cache decorator(dict to set max no. of entries

--- config.py
+++ config.py
@@ -1,11 +1,15 @@
 from pydantic import BaseSettings
 
+from functools import lru_cache
+
 # Settings is subclass of BaseSettings
 class Settings(BaseSettings):
     env_name: str = "local"
     base_url: str = "http://localhost:8000"
     db_url: str = "sqlite:///./fast_api_url_shortener.db"
 
+
+@lru_cache
 def get_settings() -> Settings:
     settings = Settings()
     print(f"Loading settings for: {settings.env_name}")
