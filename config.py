99522e45a93e4c9089c7786e2ea46c792b995b0b
paul <paulbrowne2@gmail.com>
2022-05-23 22:48:19 +0100

    first commit

---
+++ config.py
@@ -0,0 +1,12 @@
+from pydantic import BaseSettings
+
+# Settings is subclass of BaseSettings
+class Settings(BaseSettings):
+    env_name: str = "local"
+    base_url: str = "http://localhost:8000"
+    db_url: str = "sqlite:///./fast_api_url_shortener.db"
+
+def get_settings() -> Settings:
+    settings = Settings()
+    print(f"Loading settings for: {settings.env_name}")
+    return settings
