3beb8dd588a57fab0cf086cfecfeff99f10981f2
paul <paulbrowne2@gmail.com>
2022-05-24 13:21:44 +0100

    POST endpoint created using URLBase schema

--- shortener_app/main.py
+++ shortener_app/main.py
@@ -1,6 +1,18 @@
-from fastapi import FastAPI
+import validators
+from fastapi import FastAPI, HTTPException
+
+from . import schemas
 
 app = FastAPI()
+
+def raise_bad_request(message):
+    raise HTTPException(status_code=400, detail=message)
+
+@app.post("/url")
+def create_url(url: schemas.URLBase):
+    if not validators.url(url.target_url):
+        raise_bad_request(message="Your provided URL is not valid")
+    return f"TODO: Create database entry for: {url.target_url}"
 
 @app.get("/")
 def read_root():
